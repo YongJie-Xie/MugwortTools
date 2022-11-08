d7192e0a54c1337ef453c6a9464d005b7c71cf7d
YongJie-Xie <fsswxyj@qq.com>
2022-11-09 02:07:36 +0800

    【功能】导入模块失败直接退出程序

--- src/mugwort/crypto/__init__.py
+++ src/mugwort/crypto/__init__.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 
+
 try:
     import cryptography
 except ImportError:
+    import sys
+
     print('Module [mugwort.crypto.*] not imported, execute `pip install cryptography` will import it')
+    sys.exit(1)
 else:
     from .aes import AESCryptor
     from .des import TripleDESCryptor
--- src/mugwort/proxy/__init__.py
+++ src/mugwort/proxy/__init__.py
@@ -5,6 +5,9 @@ try:
     import yaml
     import apscheduler
 except ImportError:
+    import sys
+
     print('Module [mugwort.proxy.clash] not imported, execute `pip install apscheduler requests pyyaml` will import it')
+    sys.exit(1)
 else:
     from .clash import ClashProxy, ClashConfig
