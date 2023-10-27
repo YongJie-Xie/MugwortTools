a682db6e7318d686719f538bd4fb73c4469bd433
YongJie-Xie <fsswxyj@qq.com>
2023-10-27 15:14:01 +0800

    【功能】杂项：增加乱码恢复辅助工具

--- src/mugwort/basic/__init__.py
+++ src/mugwort/basic/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from .counter import Counter
 from .logger import Logger
+from .misc import codecs, recovery_garbled_text
 from .misc import get_filesize_for_human
 from .misc import get_iso8601_from_datetime
 from .misc import get_iso8601_from_timestamp
@@ -10,6 +11,8 @@ from .multitask import MultiTask, MultiTaskVariable
 __all__ = [
     'Counter',
     'Logger',
+    'codecs',
+    'recovery_garbled_text',
     'get_filesize_for_human',
     'get_iso8601_from_datetime',
     'get_iso8601_from_timestamp',
--- src/mugwort/basic/misc.py
+++ src/mugwort/basic/misc.py
@@ -10,6 +10,7 @@
 @Software    : PyCharm
 @Version     : 1.0.1
 """
+import itertools
 import typing as t
 from datetime import datetime, timezone, timedelta
 
@@ -18,6 +19,8 @@ __all__ = [
     'get_iso8601_now',
     'get_iso8601_from_datetime',
     'get_iso8601_from_timestamp',
+    'codecs',
+    'recovery_garbled_text',
 ]
 
 
@@ -113,3 +116,41 @@ def get_iso8601_from_timestamp(
         raise ValueError('时间戳超出处理范围')
     utc_datetime = datetime.utcfromtimestamp(timestamp).replace(tzinfo=timezone(timedelta(hours=0)))
     return utc_datetime.astimezone(tz=tz).isoformat()
+
+
+codecs: t.List[str] = [
+    # See: https://docs.python.org/3/library/codecs.html
+    'ascii', 'big5', 'big5hkscs', 'cp037', 'cp273', 'cp424', 'cp437', 'cp500', 'cp720', 'cp737', 'cp775', 'cp850',
+    'cp852', 'cp855', 'cp856', 'cp857', 'cp858', 'cp860', 'cp861', 'cp862', 'cp863', 'cp864', 'cp865', 'cp866', 'cp869',
+    'cp874', 'cp875', 'cp932', 'cp949', 'cp950', 'cp1006', 'cp1026', 'cp1125', 'cp1140', 'cp1250', 'cp1251', 'cp1252',
+    'cp1253', 'cp1254', 'cp1255', 'cp1256', 'cp1257', 'cp1258', 'euc_jp', 'euc_jis_2004', 'euc_jisx0213', 'euc_kr',
+    'gb2312', 'gbk', 'gb18030', 'hz', 'iso2022_jp', 'iso2022_jp_1', 'iso2022_jp_2', 'iso2022_jp_2004', 'iso2022_jp_3',
+    'iso2022_jp_ext', 'iso2022_kr', 'latin_1', 'iso8859_2', 'iso8859_3', 'iso8859_4', 'iso8859_5', 'iso8859_6',
+    'iso8859_7', 'iso8859_8', 'iso8859_9', 'iso8859_10', 'iso8859_11', 'iso8859_13', 'iso8859_14', 'iso8859_15',
+    'iso8859_16', 'johab', 'koi8_r', 'koi8_t', 'koi8_u', 'kz1048', 'mac_cyrillic', 'mac_greek', 'mac_iceland',
+    'mac_latin2', 'mac_roman', 'mac_turkish', 'ptcp154', 'shift_jis', 'shift_jis_2004', 'shift_jisx0213', 'utf_32',
+    'utf_32_be', 'utf_32_le', 'utf_16', 'utf_16_be', 'utf_16_le', 'utf_7', 'utf_8', 'utf_8_sig',
+]
+
+
+def recovery_garbled_text(
+        text: str,
+        flag: t.Union[str, t.List[str]] = None,
+) -> t.Iterable[t.Tuple[str, str, str]]:
+    """
+    乱码恢复辅助工具
+
+    :param text: 待恢复的乱码文本
+    :param flag: 用于过滤的已知文本或已知文本列表
+    :return 编码器、解码器、已恢复文本
+    """
+    targets: t.Optional[t.List[str]] = [flag] if isinstance(flag, str) else flag
+    for codec1, codec2 in itertools.permutations(codecs, 2):
+        try:
+            fixed_text = text.encode(codec1).decode(codec2)
+        except UnicodeError:
+            continue
+        else:
+            if targets and not all(target in fixed_text for target in targets):
+                continue
+            yield codec1, codec2, fixed_text
